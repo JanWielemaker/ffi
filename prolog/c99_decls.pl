dd7839fd7873874505ba6b70c9dc2e849529e210
Jan Wielemaker <j.wielemaker@cwi.nl>
2017-12-05 15:28:33 +0100

    Test, get prototypes

--- prolog/c99_decls.pl
+++ prolog/c99_decls.pl
@@ -1,15 +1,31 @@
 :- module(c99_decls,
-          [ c99_types/2                         % +Header, -AST
+          [ c99_header_ast/2,                   % +Header, -AST
+            c99_types/3                         % +Header, +Functions, -AST
           ]).
 :- use_module(library(process)).
 :- use_module(library(pure_input)).
+:- use_module(library(apply)).
 :- use_module(c99_phrase).
 
-%!  c99_types(+Header, +Symbols)
+%!  c99_types(+Header, +Functions, -AST)
 %
 %
 
-c99_types(Header, AST) :-
+c99_types(Header, Functions, Types) :-
+    c99_header_ast(Header, AST),
+    maplist(prototype(AST), Functions, Types).
+
+prototype(AST, Func, FuncDecl) :-
+    FuncDecl = decl(_,
+                    [ declarator(_, dd(Func, _DDS))
+                    ],
+                    _Attributes),
+    memberchk(FuncDecl, AST).
+
+
+%!  c99_header_ast(+Header, -AST)
+
+c99_header_ast(Header, AST) :-
     setup_call_cleanup(
         open_gcc_cpp(Header, In),
         phrase_from_stream(c99_parse(AST), In),
--- test/test_c99.pl
+++ test/test_c99.pl
@@ -1,2 +1,38 @@
+:- use_module('../prolog/c99_tokens').
 :- use_module('../prolog/c99_phrase').
+:- use_module('../prolog/c99_decls').
+
+tmath :-
+    c99_types("#include <math.h>",
+              [ sin,cos ], AST),
+    pp(AST).
+
+tstat :-
+    c99_types("#include <sys/types.h>
+               #include <sys/stat.h>
+               #include <unistd.h>",
+              [ stat ], AST),
+    pp(AST).
+
+t(N) :-
+    p(N, P),
+    phrase(c99_parse(AST), P),
+    pp(AST).
+
+p(1, `
+double sin(double x);
+`).
+
+p(2, `
+`).
+p(3, `
+`).
+p(4, `
+`).
+
+
+
+
+
+
 
