dfffa7e44785e82a0b22e3383c3313f96c2b7d47
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-12-22 14:08:44 +0100

    Additional tests

--- test/test_c99.pl
+++ test/test_c99.pl
@@ -9,9 +9,30 @@
             [ 'libc.so.6' ],
             [ stat(+string,-struct(stat),[-int])]).
 
+:- c_import("#include <sys/vfs.h>",
+            [ 'libc.so.6' ],
+            [ statfs(+string, -struct(statfs), [-int]) ]).
+
 :- c_import("#include <math.h>",
             [ 'libm.so.6' ],
             [ sin(+double, [-double]) ]).
+
+:- c_import("#include \"test/test.c\"",
+            [ 'test/test.so' ],
+            [ get_point(-struct(point), [-int])
+            ]).
+
+sfs(File, Data) :-
+    c_alloc_string(S, File, text),
+    c_struct_alloc(Data, statfs),
+    pp(Data),
+    statfs(S, Data, Status),
+    writeln(Status).
+
+pt(Pt) :-
+    c_struct_alloc(Pt, point),
+    get_point(Pt, Status),
+    writeln(Status).
 
 tmath :-
     c99_types("#include <math.h>",
@@ -25,10 +46,21 @@ tstat :-
               [ stat ], AST),
     pp(AST).
 
+tstatfs :-
+    c99_types("#include <sys/vfs.h>",
+              [ statfs ], AST),
+    pp(AST).
+
+tpt :-
+    c99_types("#include \"test/test.c\"",
+              [ get_point ], AST),
+    pp(AST).
+
+
 tstat_ast(AST) :-
     c99_header_ast("#include <sys/types.h>
-                   #include <sys/stat.h>
-                   #include <unistd.h>",
+                    #include <sys/stat.h>
+                    #include <unistd.h>",
                    AST).
 
 tstrcpy :-
@@ -47,8 +79,12 @@ double sin(double x);
 `).
 
 p(2, `
+extern int __fpclassifyf128 (double __value) __attribute__ ((__nothrow__ , __leaf__))
+     __attribute__ ((__const__));
 `).
 p(3, `
+extern int __fpclassifyf128 (_Float128 __value) __attribute__ ((__nothrow__ , __leaf__))
+     __attribute__ ((__const__));
 `).
 p(4, `
 `).
@@ -57,6 +93,8 @@ p(4, `
 incl(AST) :-
     phrase_from_file(c99_parse(AST), 'incl.h', []).
 
+m(AST) :-
+    phrase_from_file(c99_parse(AST), 'm.cpp', []).
 
 
 
