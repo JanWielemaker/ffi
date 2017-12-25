a94f6df7e27a7b3434643ba873d4fe093673c05d
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-12-25 12:16:29 +0100

    Export c_struct preds again

--- prolog/cinvoke.pl
+++ prolog/cinvoke.pl
@@ -45,6 +45,11 @@
             c_sizeof/2,                 % +Type, -Bytes
             c_alignof/2,                % +Type, -Bytes
 
+            c_struct/2,                 % +Name, +Fields
+            c_struct_alloc/2,           % -Ptr, +Name
+            c_struct_load/3,            % +Ptr, +Field, -Value
+            c_struct_store/3,           % +Ptr, +Field, +Value
+
             c_alloc_string/3,           % -Ptr, +Data, +Encoding
             c_load_string/4,            % +Ptr, -Data, +Type, +Encoding
 
