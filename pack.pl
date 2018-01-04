8bf67528452c82e2452a424e6dc8c6f284da726a
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-01-04 15:40:51 +0100

    Include pack metadata

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(cinvoke).
+version('0.1').
+title('Dynamically call C functions').
+keywords(['C', 'foreign interface', scripting]).
+author( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+home( 'https://github.com/JanWielemaker/cinvoke.git' ).
+download( 'https://github.com/JanWielemaker/cinvoke/archive/V*.zip' ).
