429faae54a033845cd78c14f0d7d0a6a009cffae
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-01-15 22:37:42 +0100

    Turn union test into proper test suite.

--- test/test_union.pl
+++ test/test_union.pl
@@ -1,3 +1,7 @@
+:- module(test_union,
+          [ test_union/0
+          ]).
+
 :- use_module(library(plunit)).
 :- use_module('../prolog/cinvoke').
 
@@ -6,7 +10,22 @@
             [ set_d(-union(convert), +double)
             ]).
 
-t(Float, Union) :-
-    set_d(Union, Float).
+test_union :-
+    run_tests([c_union]).
 
+:- begin_tests(c_union).
+
+                                                % assumes little endian double
+test(float_bytes, Bytes == [31,-123,-21,81,-72,30,19,64]) :-
+    Float = 4.78,
+    set_d(Union, Float),
+    c_load(Union[d], Copy),
+    assertion(Copy == Float),
+    numlist(0,7,Indexes),
+    maplist(float_byte(Union), Indexes, Bytes).
+
+:- end_tests(c_union).
+
+float_byte(Union, N, B) :-
+    c_load(Union[s][N], B).
 
