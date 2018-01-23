17597852976948524bbd757a612c163a5cea8e38
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-01-23 21:31:12 +0100

    Started example that embeds Python

---
+++ examples/python.pl
@@ -0,0 +1,37 @@
+% :- module(python, []).
+:- use_module(library(ffi)).
+
+/** <module> Embed Python
+
+@see https://docs.python.org/3/extending/embedding.html
+*/
+
+:- c_import("#include <Python.h>",
+            [ '-lpython3.6m',
+              '-I/usr/include/python3.6m',
+              '-I/usr/include/x86_64-linux-gnu/python3.6m'
+            ],
+            [ 'Py_DecodeLocale'(+string, -int, [-string(wchar_t)]),
+              'Py_SetProgramName'(+string(wchar_t)),
+              'Py_Initialize'(),
+              'PyRun_SimpleStringFlags'(+string, +'PyCompilerFlags', [-int]),
+              'Py_FinalizeEx'([-int])
+            ]).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Issues:
+
+  - 'Py_DecodeLocale' accepts a NULL pointer
+  - 'Py_DecodeLocale' returns a pointer that must be freed using
+    PyMem_RawFree()
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+
+today :-
+    current_prolog_flag(os_argv, [Program|_]),
+    'Py_SetProgramName'(Program),
+    'Py_Initialize'(),
+    'PyRun_SimpleStringFlags'("from time import time,ctime\n\c
+                               print('Today is', ctime(time()))\n",
+                              null, Rc),
+    writeln(Rc).
