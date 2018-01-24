c794711d3fc994d13c84bfee550e33176d94dc49
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-01-24 13:25:58 +0100

    Document closure handling

--- test/test_funcptr.pl
+++ test/test_funcptr.pl
@@ -1,5 +1,12 @@
 :- use_module('../prolog/ffi').
 
+/** <module> Test closure handling
+
+This test demonstrates a simple closure.  Type extraction and validation
+of closures is supported, but closure   definition through libffi is not
+yet supported.
+*/
+
 :- c_import("#include \"test/test_funcptr.c\"",
             [ 'test/test_funcptr' ],
             [ test(+function(+int, [-int]), +int, [-int])
