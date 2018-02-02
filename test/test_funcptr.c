ce14608ffb216ef2abd210bb1bdaf74eb5b55b5d
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-02-02 10:04:49 +0100

    Wip dealing with struct function pointers

--- test/test_funcptr.c
+++ test/test_funcptr.c
@@ -1,7 +1,17 @@
 typedef long rtype;
 typedef rtype (*testf)(int);
 
+typedef struct funcs
+{ double (*mul_di)(double, int);
+} funcs;
+
+
 rtype
 test(testf func, int i)
 { return (func)(i);
+}
+
+double
+test_fstruct(funcs *f, double d, int i)
+{ return (*f->mul_di)(d, i);
 }
--- test/test_funcptr.pl
+++ test/test_funcptr.pl
@@ -2,12 +2,24 @@
 
 /** <module> Test closure handling
 
-This test demonstrates a simple closure.  Type extraction and validation
-of closures is supported, but closure   definition through libffi is not
-yet supported.
+This test demonstrates closure handling,  both   passing  a closure to a
+function and storing one in a struct field.
 */
 
 :- c_import("#include \"test/test_funcptr.c\"",
             [ 'test/test_funcptr' ],
-            [ test(+function(+int, [-int]), +int, [-int])
+            [ test(twice(int, [int]), int, [int]),
+              test_fstruct(struct(funcs), float, int, [float])
             ]).
+
+twice(In, Out) :-
+    Out is 2*In.
+
+t(N) :-
+    test(N, X),
+    writeln(X).
+
+tf(D,I,R) :-
+    c_alloc(FPtr, struct(funcs)),
+    c_store(FPtr[mul_di], mul_di(float, int, [float])),
+    test_fstruct(FPtr, D, I, R).
