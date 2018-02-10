d74cc1abb4682529e5768b5f0a5ce90e58fef96c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-02-10 13:43:27 +0100

    Fixed test case

--- test/test_mode.pl
+++ test/test_mode.pl
@@ -12,6 +12,8 @@ test_mode :-
 :- dynamic
     leak_iterations/1.
 
+leak_iterations(100 000).
+
 test_output_leaks(Iterations) :-
     retractall(leak_iterations(_)),
     asserta(leak_iterations(Iterations)),
@@ -26,7 +28,7 @@ test_output_leaks(Iterations) :-
               test_v_ofs(-string~free),
               test_v_oip(-(*(int))),
               test_v_ofip(-(*(int)~free)),
-              test_v_ofsp(-(*struct(point)~free)),
+              test_v_ofsp(-((*struct(point))~free)),
                                         % return
               test_s([string]),
               test_fs([string~free])
@@ -51,6 +53,10 @@ test(test_v_ofip, [E0,E1,E2] == [1,2,3]) :-
     c_load(Ptr[0], E0),
     c_load(Ptr[1], E1),
     c_load(Ptr[2], E2).
+test(test_v_ofsp, [X,Y] == [1,2]) :-
+    test_v_ofsp(Struct),
+    c_load(Struct[x], X),
+    c_load(Struct[y], Y).
                                         % RETURN
 test(test_s, S == "hello world") :-
     test_s(S).
@@ -63,6 +69,8 @@ test(test_fs, S == "hello world") :-
 
 test(test_s) :-
     test_leak(test_s(_)).
+test(test_v_ofsp) :-
+    test_leak(test_v_ofsp(_)).
 
 :- end_tests(c_mode_leak).
 
