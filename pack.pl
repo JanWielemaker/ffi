9d5483cd19d25555e8545ffac63f0dc073af51bc
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-03-29 13:56:52 +0200

    Updated version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(ffi).
-version('0.6').
+version('0.7').
 title('Dynamically call C functions').
 keywords(['C', 'foreign interface', scripting]).
 author( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
